845648e5e691ca9b9ddc12411ccfe1e0e8259c8d
Amelia Steinberger <steinba@bgsu.edu>
2020-02-04 21:27:18 -0500

    can find user's current timezone --UTC

--- five_oclock/views.py
+++ five_oclock/views.py
@@ -4,7 +4,16 @@ import datetime
 
 def isItFive(requests):
     now = datetime.datetime.now()
+
+    tz_string = datetime.datetime.now(datetime.timezone.utc).astimezone().tzname()
     if now.hour == 17:
         return HttpResponse("it's 5 o'clock there")
     else:
-        return HttpResponse("it's 5 o'clock somewhere")
+       # return HttpResponse("it's 5 o'clock somewhere")
+       return HttpResponse(tz_string)
+   
+
+    
+
+    
+

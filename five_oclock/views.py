527d61eed08e9c9277cbeffbe098a9b51e6fa4fb
Joey Albrecht <joey@joseph-albrecht.me>
2020-03-09 13:36:04 -0400

    clean up utc calculation and add new timezones.

--- five_oclock/views.py
+++ five_oclock/views.py
@@ -4,47 +4,59 @@ import datetime
 import pytz
 import re
 
+def utcOffsetWhereTheHourIs(target_hour):
+    """Return the UTC offset where the hour is currently the target_hour."""
+    
+    utc_hour   = datetime.datetime.now(datetime.timezone.utc).hour
+    utc_offset = target_hour - utc_hour
+
+    if abs(utc_offset) > 12:
+        utc_offset = goAroundTheWorldTheOtherWay(utc_offset)
+
+    return utc_offset
+
+def goAroundTheWorldTheOtherWay(offset):
+    """Because the earth is a globe, a particular timezone can be reached going
+       eastward or westward. This function returns a new utc offset going the
+       other cardinal direction.
+       Examples:
+       -14 ->  10
+        14 -> -10"""
+
+    new_magnitude = 24 - abs(offset)
+    old_direction = (offset // abs(offset))
+    new_direction = old_direction * -1
+
+    return new_magnitude * new_direction
+    
 def isItFive(requests):
-    now = datetime.datetime.now()
-    hour = int(now.hour)
+    utc_offset = utcOffsetWhereTheHourIs(17)
 
+    place = {-12: "Baker Islands",
+             -11: "Niue, New Zealand",
+             -10: "Honolulu",
+              -9: "Anchorage",
+              -8: "Vancouver",
+              -7: "Phoenix",
+              -6: "Mexico City",
+              -5: "Havana",
+              -4: "La Paz",
+              -3: "Sao Paulo",
+              -2: "South Georgia",
+              -1: "Cape Verde",
+               0: "Reykjavic",
+               1: "Brazzaville",
+               2: "Cairo",
+               3: "Mecca",
+               4: "Yerevan",
+               5: "Maldives",
+               6: "Dhaka, Bangladesh",
+               7: "Bangkok",
+               8: "Taipei",
+               9: "Seoul",
+              10: "Guam",
+              11: "Solomon Islands",
+              12: "Baker Islands"}[utc_offset]
 
-    #get UTC offset for current time
-    tz_string = datetime.datetime.now(datetime.timezone.utc).astimezone().strftime('%z')#.tzname()
-    
-    
-    #convert offset to format we need
-    if re.match("[+|-][0][0-9][0][0]$", tz_string):
-    	utc_num = tz_string[0:3:2]
-    	num = int(utc_num)
-    	difference = 17 - hour
-    	offset = num + difference
-
-    	if offset == -12:
-    		place = "Baker Islands"
-    	elif offset == -11:
-    		place = "Niue, New Zealand"
-    	elif offset == -10:
-    		place = "Honolulu"
-    	elif offset == -9:
-    		place = "Anchorage"
-    	elif offset == -8:
-    		place = "Vancouver"
-    	elif offset == -7:
-    		 place = "Phoenix"
-    	elif offset == -6:
-    		place = "Mexico City"
-    	elif offset == -5:
-    		place = "Havana"
-    	elif offset == -4:
-    		place = "La Paz"
-    	else: 
-    		place = 'not added yet...'
-
-    	# typee2 = utc_num + 1
-    	return  HttpResponse("It is 5oclock in  " + place)
-    else:
-    	utc_num = tz_string[0:3]
-    	# return HttpResponse(utc_num) 
-	
-
+    # typee2 = utc_num + 1
+    return  HttpResponse("It is 5oclock in  " + place)
